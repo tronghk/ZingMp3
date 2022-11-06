2d7653f6f1b5fc59ed990e2325b52b80abde06b2
khanhpham2103 <pk2132002@gmail.com>
2022-11-06 18:46:59 +0700

    Thử đổi 10 bài mới đầu tiên phần Nhạc mới

--- app.js
+++ app.js
@@ -209,6 +209,188 @@ const musicTopSong = [
         img: "./assets/fonts/img/DangDo.jpg"
     }
 ];
+const newSongList = [
+		{	
+			id: 0,
+			fileName: "EmThich.mp3",
+			type: "Nhac",
+			timeMax: "02:56",
+			name: "Em Thích",
+			singer: "Seen, Lửa",
+			img: "./assets/fonts/img/EmThich.jpg"
+	    	},
+		{ 
+			id: 1,
+			fileName : "HenMotMai.mp3",
+			type : "Nhac",
+			timeMax : "04:43",
+			name : "Hẹn Một Mai",
+			singer : "Bùi Tuấn Anh",
+			img : "./assets/fonts/img/HenMotMai.jpg"
+		},
+		{ 
+			id: 2,
+			fileName : "TinhDau.mp3",
+			type : "Nhac",
+			timeMax : "04:56",
+			name : "Tình Đầu",
+			singer : "Tăng Duy Tân",
+			img : "./assets/fonts/img/TinhDau.jpg"
+		},
+		{ 
+			id: 3,
+			fileName : "Yeu5.mp3",
+			type : "Nhac",
+			timeMax : "04:00",
+			name : "Yêu 5",
+			singer : "Rhymastic",
+			img : "./assets/fonts/img/Yeu5.jpg"
+		},
+		{ 
+			id: 4,
+			fileName : "AnhDanhRoiNguoiYeuNay.mp3",
+			type : "Nhac",
+			timeMax : "03:37",
+			name : "Anh Đánh Rơi Người Yêu Này",
+			singer : "Andiez",
+			img : "./assets/fonts/img/AnhDanhRoiNguoiYeuNay.jpg"
+		},
+		{ 
+			id: 5,
+			fileName : "MotNganNoiDau.mp3",
+			type : "Nhac",
+			timeMax : "05:21",
+			name : "Một Ngàn Nỗi Đau",
+			singer : "Văn Mai Hương, Hứa Kim Tuyền",
+			img : "./assets/fonts/img/MotNganNoiDau.jpg"
+		},
+		{ 
+			id: 6,
+			fileName : "ChuyenDoiTa.mp3",
+			type : "Nhac",
+			timeMax : "03:28",
+			name : "Chuyện Đôi Ta",
+			singer : "Emcee L",
+			img : "./assets/fonts/img/ChuyenDoiTa.jpg "
+		},
+		{ 
+			id: 7,
+			fileName : "DauDeTruongThanh.mp3",
+			type : "Nhac",
+			timeMax : "05:30",
+			name : "Đau Để Trưởng Thành",
+			singer : "Only C",
+			img : "./assets/fonts/img/DauDeTruongThanh.jpg"
+		},
+		{ 
+			id: 8,
+			fileName : "TuSu.mp3",
+			type : "Nhac",
+			timeMax : "04:17",
+			name : "Tự Sự (Qua Bển Làm Chi OST)",
+			singer : "Orange",
+			img : "./assets/fonts/img/TuSu.jpg"
+		},
+		{ 
+			id: 9,
+			fileName : "SuytNuaThi.mp3",
+			type : "Nhac",
+			timeMax : "04:43",
+			name : "Suýt Nữa Thì (Chuyến Đi Của Thanh Xuân OST)",
+			singer : "Andiez",
+			img : "./assets/fonts/img/SuytNuaThi.jpg"
+		},
+		{ 
+			id: 10,
+			fileName : "PhoCuConAnh.mp3",
+			type : "Nhac",
+			timeMax : "03:53",
+			name : "Phố Cũ Còn Anh",
+			singer : " Quinn, Chilly",
+			img : "./assets/fonts/img/PhoCuConAnh.jpg"
+		},
+		{ 
+			id: 11,
+			fileName : "NoAiDoLoiXinLoi.mp3",
+			type : "Nhac",
+			timeMax : "05:37",
+			name : "Nợ Ai Đó Lời Xin Lỗi",
+			singer : "Bozitt",
+			img : "./assets/fonts/img/NoAiDoLoiXinLoi.jpg"
+		},
+		{ 
+			id: 12,
+			fileName : "AnhThuongEmNhatMa.mp3",
+			type : "Nhac",
+			timeMax : "03:13",
+			name : "Anh Thương Em Nhất Mà",
+			singer : "Lã. Log, TiB",
+			img : "./assets/fonts/img/AnhThuongEmNhatMa.jpg"
+		},
+		{ 
+			id: 13,
+			fileName : "MotBuocYeuDamVanDau.mp3",
+			type : "Nhac",
+			timeMax : "04:58",
+			name : "Một Bước Yêu Dặm Vạn Đau",
+			singer : "Mr.Siro",
+			img : "./assets/fonts/img/MotBuocYeuDamVanDau.jpg"
+		},
+		{ 
+			id: 14,
+			fileName : "BoEmVaoBalo.mp3",
+			type : "Nhac",
+			timeMax : "03:12",
+			name : "Bỏ Em Vào Balo",
+			singer : "Tân Trần, Freak D",
+			img : "./assets/fonts/img/BoEmVaoBalo.jpg"
+		},
+		{ 
+			id: 15,
+			fileName : "CoHenVoiThanhXuan.mp3",
+			type : "Nhac",
+			timeMax : "03:38",
+			name : "Có Hẹn Với Thanh Xuân",
+			singer : "MONSTAR",
+			img : "./assets/fonts/img/CoHenVoiThanhXuan.jpg"
+		},
+		{ 
+			id: 16,
+			fileName : "TinhYeuKhungLong.mp3",
+			type : "Nhac",
+			timeMax : "03:10",
+			name : "Tình Yêu Khủng Long",
+			singer : "Fay",
+			img : "./assets/fonts/img/TinhYeuKhungLong.jpg"
+		},
+		{ 
+			id: 17,
+			fileName : "TrenTinhBanDuoiTinhYeu.mp3",
+			type : "Nhac",
+			timeMax : "03:19",
+			name : "Trên Tình Bạn Dưới Tình Yêu",
+			singer : "MIN",
+			img : "./assets/fonts/img/TrenTinhBanDuoiTinhYeu.jpg"
+		},
+		{ 
+			id: 18,
+			fileName : "MotNha.mp3",
+			type : "Nhac",
+			timeMax : "03:05",
+			name : "Một Nhà",
+			singer : "Da LAB",
+			img : "./assets/fonts/img/MotNha.jpg"
+		},
+		{ 
+			id: 19,
+			fileName : "MinhCungNhauDongBang.mp3",
+			type : "Nhac",
+			timeMax : "04:12",
+			name : "Mình Cùng Nhau Đóng Băng",
+			singer : "Thùy Chi",
+			img : "./assets/fonts/img/MinhCungNhauDongBang.jpg"
+		}
+	];
 var music;
 //sự kiện trái tim
 let isPlaySong = true;
@@ -806,7 +988,7 @@ function changeListMusic(obj){
             listSong = document.querySelectorAll(".newSong .perform_Song-List .perform_Song-cart");
             listIdSong = document.querySelectorAll(".newSong .song-cart-introduce-img i");
             listGitPlay = document.querySelectorAll(".newSong .gitPlay");
-            // music = ;
+            music = newSongList;
             break;
         }
         default: {break;};
